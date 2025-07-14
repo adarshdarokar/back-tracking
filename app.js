d73907ea0ac24ddc6f45db3195f08c139bfa318c
adarsh darokar <adarshdarokar55@gmail.com>
2025-07-14 18:34:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var solve = function (nums, ans, cur, i) {
+  if (i == nums.length) {
+    ans.push([...cur]);
+    return;
+  }
+  cur.push(nums[i]);
+  solve(nums, ans, cur, i + 1);
+  cur.pop();
+
+  solve(nums, ans, cur, i + 1);
+};
+var subsets = function (nums) {
+  let ans = [];
+  let cur = [];
+  solve(nums, ans, cur, 0);
+  return ans;
+};
+console.log(subsets([1,2,3]));
